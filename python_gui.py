55f901adddec38368f1dd01b97cb60bb109c9d2a
Ricardo Trevino <rickytrevino2380@gmail.com>
2024-10-09 16:40:19 -0700

    init + some starting code (system settings color)

---
+++ python_gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import customtkinter 
+from pytube import YouTube as yt
+
+#sys settings 
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# our app frame
+
+app = customtkinter.CTk()
