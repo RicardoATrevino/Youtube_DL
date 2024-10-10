d7da1d4e1b1c469d5145663436b355f68fb04181
Ricardo Trevino <rickytrevino2380@gmail.com>
2024-10-09 23:04:59 -0700

    app now functions as intended, downloads mp3 files into a file if your choosing.

--- Main.py
+++ Main.py
@@ -17,9 +17,12 @@ default_settings = {
 
 #Load settings from the JSON file. or create if non existant
 if os.path.exists(settings_file):
+    print(f"Settings file found: {settings_file}")
     with open(settings_file, 'r') as file:
         settings = json.load(file)
+        print("Settings file loaded successfully.")
 else:
+    print(f"Settings file not found. Creating new settings file at: {settings_file}")
     settings = default_settings
     with open(settings_file, 'w') as file:
         json.dump(default_settings, file)
@@ -45,7 +48,7 @@ def file_select():
 
 def startDownload():
     folder = settings.get('download_folder', None)
-    print(folder)
+    
     if not folder:
         folder = file_select()
     if folder:
@@ -69,11 +72,6 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 
-# Load Settings
-
-
-
-
 #app frame
 
 app = customtkinter.CTk()
@@ -82,8 +80,8 @@ app.title("Youtube Downloader")
 
 #adding ui elements
 
-title = customtkinter.CTkLabel(app, text="insert link")
-title.pack(padx=10, pady=10)
+title = customtkinter.CTkLabel(app, text="Insert link")
+title.pack(padx=10, pady=5)
 
 #link input
 url_var = tkinter.StringVar()
@@ -91,26 +89,33 @@ link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
 
+#download options
+
+videoOption = customtkinter.CTkButton(app, text="Click here for Video")
+videoOption.pack(padx=20, pady=10)
+
+audioOption = customtkinter.CTkButton(app, text="Click here for just audio" )
+audioOption.pack(padx=15, pady=10)
+
+
+
+
 # Folder Selection Button and Label
 folder_button = customtkinter.CTkButton(app, text="Select Download Folder", command=file_select)
 folder_button.pack(pady=10)
 
-folder_label = customtkinter.CTkLabel(app, text="No folder selected.")
+loaded_folder = settings.get('download_folder', "No folder selected.")  # Load saved folder or default
+folder_label = customtkinter.CTkLabel(app, text=f"Download folder: {loaded_folder}")
 folder_label.pack(pady=10)
 
 # Status Label
 status_label = customtkinter.CTkLabel(app, text="")
-status_label.pack(pady=10)
+status_label.pack()
 
 #download button
 
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
-download.pack(padx = 10, pady=10)
-
-#download options
-
-videoOption = customtkinter.CTkButton(app)
-
+download.pack(padx = 10, pady=5)
 
 #finished downloading
 
